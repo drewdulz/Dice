78739d7a0d94ae73e9e17bcc39be9b3b7744c60f
Drew Warkentin <drewdulz@gmail.com>
2014-01-22 19:49:24 -0500

    Styled app and added screenshots
    This is a stable version ready for release. Future versions will
    include a rolling animation, roll history, and the choice of multiple
    dice.

--- src/dice.c
+++ src/dice.c
@@ -5,6 +5,7 @@
  
  */
 
+// Pebble screen size is 144 x 168 pixels.
 
 #include <pebble.h>
 #include <pebble_fonts.h>
@@ -12,65 +13,115 @@
 
 static Window *window;
 static TextLayer *instruction_layer;
+static TextLayer *count_layer;
 static TextLayer *message_layer;
+static TextLayer *intro_layer;
+static BitmapLayer *dice_view_layer;
+static GBitmap *image;
+
+
 
 int num_rolls = 0;
 char counter_text[5]; //Can count up to 999 (3 digits plus signs & \0)
+int roll;
 
 
-// Random number generator -- Still need to seed with the time
-int random()
+// Random number generator
+int randomnum()
 {
     return (rand() % 6);
 }
 
+// Function used for displaying the counter
 void update_text_layer_with_int(int num, TextLayer* text_layer){
     snprintf(counter_text, (5)*sizeof(char), "%d", num);
     text_layer_set_text(text_layer, counter_text);
 }
 
-// Get number as Char so it can be displayed as a text layer
-char *nums[6] = {"1","2","3","4","5","6"};
 
-void dice_draw(Layer *message_layer, GContext *context) {
-    graphics_context_set_fill_color(context, GColorBlack);
-    graphics_fill_rect(context, GRect(5,5,5,5), 0, GCornerNone);
-    graphics_context_set_fill_color(context, GColorWhite);
-    graphics_fill_circle(context, GPoint(10,10), 4);
+// Display image of dice roll
+void display_roll(int roll){
+    Layer *window_layer = window_get_root_layer(window);
+
+    if (num_rolls==0) {
+        dice_view_layer = bitmap_layer_create(GRect(0,0,144,105));
+        bitmap_layer_set_background_color(dice_view_layer, GColorBlack);
+        bitmap_layer_set_alignment(dice_view_layer, GAlignCenter);
+        layer_add_child(window_layer, bitmap_layer_get_layer(dice_view_layer));
+
+        // Vibrate long pulse
+        vibes_short_pulse();
+        
+        if (roll==1) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE1);
+        } else if (roll==2) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE2);
+        } else if (roll==3) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE3);
+        } else if (roll==4) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE4);
+        } else if (roll==5) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE5);
+        } else {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE6);
+        }
+        
+        bitmap_layer_set_bitmap(dice_view_layer, image);
+        
+    } else {
+
+        // Setup dice_view_layer where dice image is displayed
+        gbitmap_destroy(image);
+        
+        // Vibrate long pulse
+        vibes_short_pulse();
+    
+        if (roll==1) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE1);
+        } else if (roll==2) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE2);
+        } else if (roll==3) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE3);
+        } else if (roll==4) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE4);
+        } else if (roll==5) {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE5);
+        } else {
+            image = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_DICE6);
+        }
+
+        bitmap_layer_set_bitmap(dice_view_layer, image);
+    }
 }
+
 
 void accel_tap_handler(AccelAxisType axis, int32_t direction) {
     if(ACCEL_AXIS_Y == 1 || ACCEL_AXIS_Y == -1){
-        //Output random number on wrist shake
-        text_layer_set_text(message_layer, nums[random()]);
-        //update counter
+        roll = randomnum();
+        // Output the random number via dice image
+        display_roll(roll);
         num_rolls += 1;
-        update_text_layer_with_int(num_rolls,instruction_layer);
+        update_text_layer_with_int(num_rolls,count_layer);
         
     }
 }
 
-// This function used for manual override. It's done through the accelerometer.
+// This function used for manual override. It's normally done through the accelerometer.
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    // Output the random number
-    text_layer_set_text(message_layer, nums[random()]);
+    roll = randomnum();
+    // Output the random number via dice image
+    display_roll(roll);
     num_rolls += 1;
-    //text_layer_set_text(instruction_layer, num_rolls);
-    update_text_layer_with_int(num_rolls,instruction_layer);
-    //Draw a rectangle
-    //dice_draw(message_layer, context);
-    //graphics_context_set_stroke_color(context, GColorBlack);
-    //graphics_context_set_fill_color(context, GColorBlack);
-    //graphics_draw_round_rect(context, GRect(5,5,5,5), 2);
-    //graphics_draw_circle(context, GPoint(10,10), 5);
+    update_text_layer_with_int(num_rolls,count_layer);
+    
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(message_layer, " ");
+    // Insert up button stuff here
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    text_layer_set_text(message_layer, " ");
+    // Insert down button stuff here
 }
 
 static void click_config_provider(void *context) {
@@ -82,34 +133,41 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
     
-    
-    message_layer = text_layer_create(GRect(0,0,144,100));
-    text_layer_set_background_color(message_layer, GColorWhite);
-    text_layer_set_text_color(message_layer, GColorBlack);
-    text_layer_set_text_alignment(message_layer, GTextAlignmentCenter);
-    text_layer_set_overflow_mode(message_layer, GTextOverflowModeWordWrap);
-    // Set the font size and type
-    text_layer_set_font(message_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
-    layer_add_child(window_layer, text_layer_get_layer(message_layer));
-    
-    
-    instruction_layer = text_layer_create(GRect(0,100,144,60));
+    // Setup the instruction layer
+    instruction_layer = text_layer_create(GRect(0,105,120,55));
     text_layer_set_background_color(instruction_layer, GColorBlack);
     text_layer_set_text_color(instruction_layer, GColorWhite);
-    text_layer_set_text(instruction_layer, "Shake wrist to\nrole the die.");
-    text_layer_set_text_alignment(instruction_layer, GTextAlignmentCenter);
+    text_layer_set_text(instruction_layer, "Roll number:  ");
+    text_layer_set_text_alignment(instruction_layer, GTextAlignmentRight);
     text_layer_set_overflow_mode(instruction_layer, GTextOverflowModeWordWrap);
-    // Set the font size and type
-    text_layer_set_font(instruction_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+    text_layer_set_font(instruction_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
     layer_add_child(window_layer, text_layer_get_layer(instruction_layer));
     
-    //Draw the square
-    //message_layer->update_proc = dice_draw;
+    // Setup the count layer (where the count is displayed)
+    count_layer = text_layer_create(GRect(120,105,24,55));
+    text_layer_set_background_color(count_layer, GColorBlack);
+    text_layer_set_text_color(count_layer, GColorWhite);
+    text_layer_set_text(count_layer, "0");
+    text_layer_set_text_alignment(count_layer, GTextAlignmentLeft);
+    text_layer_set_overflow_mode(count_layer, GTextOverflowModeWordWrap);
+    text_layer_set_font(count_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+    layer_add_child(window_layer, text_layer_get_layer(count_layer));
+    
+    // Setup intro layer
+    intro_layer = text_layer_create(GRect(0,0,144,105));
+    text_layer_set_background_color(intro_layer, GColorBlack);
+    text_layer_set_text_color(intro_layer, GColorWhite);
+    text_layer_set_text(intro_layer, "Shake to Roll!");
+    text_layer_set_text_alignment(intro_layer, GTextAlignmentCenter);
+    text_layer_set_overflow_mode(intro_layer, GTextOverflowModeWordWrap);
+    text_layer_set_font(intro_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    layer_add_child(window_layer, text_layer_get_layer(intro_layer));
 }
 
 static void window_unload(Window *window) {
     text_layer_destroy(instruction_layer);
     text_layer_destroy(message_layer);
+    bitmap_layer_destroy(dice_view_layer);
     
 }
 
@@ -129,6 +187,7 @@ static void init(void) {
 
 static void deinit(void) {
     window_destroy(window);
+    bitmap_layer_destroy(dice_view_layer);
 }
 
 int main(void) {
